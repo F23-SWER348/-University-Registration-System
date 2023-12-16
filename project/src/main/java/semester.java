b9a21da34706bf8c416968ea7dc1d4c088e5e7b2
NourAlhuda03 <nourabunassar93.gmail.com>
2023-12-16 20:32:31 +0200

    edit2

--- project/src/main/java/semester.java
+++ project/src/main/java/semester.java
@@ -5,10 +5,10 @@ import java.util.Map;
 public class semester {
     private String name;
     private int year;
-    private List<course> courses;
+    private Map<String, course> courses;
 
 //add Constructors
-    public semester(String name, int year, List< course> courses) {
+    public semester(String name, int year, Map<String, course> courses) {
         this.name = name;
         this.year = year;
         this.courses = courses;
@@ -21,31 +21,64 @@ public class semester {
     public int getYear() {
         return year;
     }
-    public List<course> getCourses() {
+    public Map<String, course> getCourses() {
         return courses;
     }
 
+    public void setCourses(Map<String, course> courses) {
+        this.courses = courses;
+    } 
 
         // Method to add a course to the semester
     public void addCourse(course course) {
-        courses.add( course);
+        courses.put(course.getName(), course);
     }
     
     // Method to get a course by name
-    public void removeCourse(course courseName) {
-         courses.remove(courseName);
+    public course getCourseByName(String courseName) {
+        return courses.get(courseName);
+    }
+
+
+    // Method to view prerequisites for a course
+    public List<course> viewPrerequisites(String courseName) {
+        course course = getCourseByName(courseName);
+        if (course != null) {
+            return course.getPrerequisites();
+        }
+        return Collections.emptyList();
+    }
+
+    public void addCourse(String courseCode, course course) {
+        courses.put(courseCode, course);
+    }
+
+    public void removeCourse(String courseCode) {
+        courses.remove(courseCode);
+    }
+
+    public boolean hasCourse(String courseCode) {
+        return courses.containsKey(courseCode);
+    }
+
+    public course getCourse(String courseCode) {
+        return courses.get(courseCode);
     }
 
 
     public int getTotalCredits() {
-        return courses.parallelStream().mapToInt(course::getCredits).sum();
+        return courses.values().stream()
+                .mapToInt(course::getCredits)
+                .sum();
     }
 
 // method to display Semester Information
     public void displaySemesterInfo() {
         System.out.println("Semester: " + name + " " + year);
         System.out.println("Courses:");
-        courses.forEach(e -> System.out.println( e.getName()));
+        courses.forEach((code, course) ->
+                System.out.println(code + ": " + course.getName())
+        );
         System.out.println("Total Credits: " + getTotalCredits());
     }
      
