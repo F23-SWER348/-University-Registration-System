e8d3c2ce016f49661501dce18b03fd9e09501686
Zainab Atwa <engzainabatwa1@gmail.com>
2023-12-23 13:58:15 +0300

    AddTestGetState

--- project/src/test/java/stuTest.java
+++ project/src/test/java/stuTest.java
@@ -14,4 +14,17 @@ public class StuTest {
         assertEquals(avg,91.5);
     } 
 
+    @Test
+    public void testGetStateOfStudent(){
+
+        student stu1 = new student("John", "Science");
+        stu1.addGrade(new course("Swer141", 3, "Science"),95.0);
+        stu1.addGrade(new course("Swer142", 3, "Science"),88.0);  
+
+        assertEquals(stu1.getState(),"Dean's list");
+
+    }
+
+
+
 }
