671a4e1c60b20695ae9df8fb2864d1d706f1f951
Zainab Atwa <engzainabatwa1@gmail.com>
2024-01-05 01:09:17 +0300

    state

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -16,44 +16,50 @@ public class Main {
             Studentt student = new Studentt("John Doe", "john.doe@example.com");
     
             // Creating a course
-            Courset course = new Courset("Math 101", 3);
-
-            // Adding a weekly meeting to the course
-            course.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(11, 0)));
-
-                    // Creating a course
-            Courset course11 = new Courset("Math 101", 3);
-
-            // Adding a weekly meeting to the course
-            course11.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(3, 0), LocalTime.of(4, 0)));
-
+            Courset course1 = new Courset("Math 101", 3);
+            course1.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(3, 0))); // Adding a weekly meeting to the course
+            course1.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(2, 0), LocalTime.of(4, 0)));
 
             // Creating a course
-            Courset course2 = new Courset("English", 3);
-    
-            // Adding a weekly meeting to the course
-            course2.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0)));
+            Courset course2 = new Courset("Math 101", 3);
+            course2.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(1, 40), LocalTime.of(2, 0)));
+
+            // Creating a course
+            Courset course3 = new Courset("English", 3);
+            course3.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0)));
+
+            // Creating a course
+            Courset course5 = new Courset("Arabic120", 3);
+            course5.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.SUNDAY, LocalTime.of(1, 0), LocalTime.of(2, 0)));
   
-
-
-            // Adding the course to the student's schedule
-            student.addCourse(course);
+            // Adding the courses to the student's schedule
+            student.addCourse(course1);
             student.addCourse(course2);
-            student.addCourse(course11);
+            student.addCourse(course3);
+            student.addCourse(course5);
 
             // Displaying the student's weekly schedule
             student.printWeeklySchedule();
+            student.completeCourse(course1, 0.0);
+            student.completeCourse(course3, 3.0);
+            student.completeCourse(course5, 2.2);
 
+            System.out.println(student.calculateGPA()); 
+            System.out.println(student.getStudentStatus()); 
 
+            // Creating a faculty
              Faculty fac1=new Faculty("Mohammad", "123@gmail.com");
-              // Creating a course
-            Courset courses = new Courset("Math 101", 3);
 
-            // Adding a weekly meeting to the course
-            courses.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(11, 0)));
-            fac1.addTeachingCourse(courses);
-            fac1.addTeachingCourse(courses);
-            System.out.println(fac1.getWeeklyMeetingSchedule());
+             // Creating a course
+             Courset course4 = new Courset("Math 141", 3);
+             course4.addWeeklyMeeting(new WeeklyMeeting(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(11, 0)));
+
+             // Adding the courses to the faculty's schedule
+             fac1.addTeachingCourse(course4);
+             fac1.addTeachingCourse(course4); 
+
+             // Displaying the faculty's weekly schedule
+             System.out.println(fac1.getWeeklyMeetingSchedule());
 
 
 
